bbfccc92610551fb469291ec434506e6940ce32c
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-22 21:08:56 +0900

    Added an ImageField to the Post Model

--- my_site/settings.py
+++ my_site/settings.py
@@ -119,9 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+MEDIA_ROOT = BASE_DIR / "uploads"
+MEDIA_URL = "/files/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
