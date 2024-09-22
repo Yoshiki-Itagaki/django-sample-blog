a54cef2382220e271627973786e6a2f53b038e05
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-23 00:30:55 +0900

    Implemented Comment Form Submission

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from typing import Any
-from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import ListView
+from django.views import View
 
 from .models import Post
 from .forms import CommentForm
@@ -27,13 +28,32 @@ class AllPostView(ListView):
     context_object_name = "all_posts"
     
     
-class SinglePostView(DetailView):
-    template_name = "blog/post-detail.html"
-    model = Post
-    
-    def get_context_data(self, **kwargs: Any):
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
+class SinglePostView(View):   
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm()
+        }        
+        return render(request, "blog/post-detail.html", context)        
+        
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)        
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+        
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form
+        }
+        return render(request, "blog/post-detail.html", context)        
+        
  
