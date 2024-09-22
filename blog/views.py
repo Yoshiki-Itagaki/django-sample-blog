0be559446a88a297417b1f0c0923ee833a432b0d
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-22 22:08:38 +0900

    Converting Views to Class-Based ones

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,37 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
 # Create your views here.
 
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3]
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
     
-    for p in latest_posts:
-        print(p.image)
-   
-    return render(request, "blog/index.html", {
-        "posts": latest_posts
-    })
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/all-posts.html", {
-        "all_posts": all_posts
-    })
 
-def post_detail(request, slug):
-    matching_post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html", {
-        "post": matching_post,
-        "post_tags": matching_post.tags.all()
-    })
+class AllPostView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
+    
+    
+class SinglePostView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
+    
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        context["post_tags"] = self.object.tags.all()
+        return context
+ 
