2279dc8e54a6fc6eb1fdaf29e02af8c83738efed
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-22 22:59:13 +0900

    Applied a Comment Model

--- blog/models.py
+++ blog/models.py
@@ -33,3 +33,9 @@ class Post(models.Model):
     def __str__(self) -> str:
         return f"{self.title}: {self.excerpt}"
     
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
