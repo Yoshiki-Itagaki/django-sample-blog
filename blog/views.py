f7b8c9846210bd4c91c84fbeb047254295a44e12
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-23 02:48:42 +0900

    Output comments

--- blog/views.py
+++ blog/views.py
@@ -34,7 +34,8 @@ class SinglePostView(View):
         context = {
             "post": post,
             "post_tags": post.tags.all(),
-            "comment_form": CommentForm()
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id")
         }        
         return render(request, "blog/post-detail.html", context)        
         
@@ -52,7 +53,8 @@ class SinglePostView(View):
         context = {
             "post": post,
             "post_tags": post.tags.all(),
-            "comment_form": comment_form
+            "comment_form": comment_form,
+            "comments": post.comments.all().order_by("-id")
         }
         return render(request, "blog/post-detail.html", context)        
         
